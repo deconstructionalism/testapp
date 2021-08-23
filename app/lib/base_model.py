d6ac99a1c98e921f8245f55ee8ac694a1dc91389
Arjun Ray <deconstructionalism@gmail.com>
2021-08-23 05:28:16 -0400

    feat: create second base model; one for named pk, one for incr pk

--- app/lib/base_model.py
+++ app/lib/base_model.py
@@ -1,15 +1,25 @@
 from app.database import db
+from sqlalchemy_serializer import SerializerMixin
 
 
-class BaseModel(db.Model):
+class BaseModel(db.Model, SerializerMixin):
     """Abstract base model for all postgres sqlalchemy models."""
 
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
+    # columns
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
         db.DateTime,
         default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp(),
     )
+
+
+class NamedBaseModel(BaseModel, SerializerMixin):
+    """Abstract base model for all postgres sqlalchemy models."""
+
+    __abstract__ = True
+
+    # columns
+    name = db.Column(db.String(), primary_key=True)
