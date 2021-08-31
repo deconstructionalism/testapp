c46b9ec021a482123b6d19409f160c984e564635
Arjun Ray <deconstructionalism@gmail.com>
2021-08-31 08:05:10 -0400

    feat: finalize filter functionality
    - fix for all entity levelsl; will filter out
      entities based on filter database rows

--- app/summarize/controllers.py
+++ app/summarize/controllers.py
@@ -6,27 +6,27 @@ summarize = Blueprint("summarize", __name__, url_prefix="/summarize")
 
 
 @summarize.route("/", methods=["GET"])
-def index_models():
-    return jsonify(SummarizerModels.get_all_models())
+def index_resources():
+    return jsonify(SummarizerModels.get_all_resources())
 
 
 @summarize.route("/<app_name>", methods=["GET"])
-def index_app_models(app_name: str):
-    app_models = SummarizerModels.get_app_models(app_name)
+def index_app_resources(app_name: str):
+    app_resources = SummarizerModels.get_app_resources(app_name)
 
-    return jsonify(app_models)
-
-
-@summarize.route("/<app_name>/<model_name>", methods=["GET"])
-def show_model(app_name: str, model_name: str):
-    model = SummarizerModels.get_model(app_name, model_name)
-
-    return jsonify(model)
+    return jsonify(app_resources)
 
 
-@summarize.route("/<app_name>/<model_name>/<field_name>", methods=["GET"])
-def show_field(app_name: str, model_name: str, field_name: str):
-    field = SummarizerModels.get_field(app_name, model_name, field_name)
+@summarize.route("/<app_name>/<resource_name>", methods=["GET"])
+def show_resource(app_name: str, resource_name: str):
+    resource = SummarizerModels.get_resource(app_name, resource_name)
+
+    return jsonify(resource)
+
+
+@summarize.route("/<app_name>/<resource_name>/<field_name>", methods=["GET"])
+def show_field(app_name: str, resource_name: str, field_name: str):
+    field = SummarizerModels.get_field(app_name, resource_name, field_name)
 
     return jsonify(field)
 
--- app/summarize/lib/summarizer_models.py
+++ app/summarize/lib/summarizer_models.py
@@ -1,61 +1,153 @@
+from app.lib.base_model import NamedBaseModel
+from app.filters.models import FieldFilter, MetadataFilter, ResourceFilter
 from app.summarize.models import Field, Resource
 from app.summarize.lib.extractors import (
-    SummarizerResource,
     SummarizerField,
+    SummarizerMetadata,
+    SummarizerResource,
 )
 from flask import abort
-from typing import List
+from typing import List, Union
+
+# HELPER FUNCTIONS
+
+
+def filter_models(
+    models: List[NamedBaseModel],
+    filter_class: Union[FieldFilter, MetadataFilter, ResourceFilter],
+):
+    """
+    For a list of named models, filter out any models who have names matching
+    the filters in a given filter class.
+    """
+    filters = filter_class.query.all()
+
+    return [
+        model
+        for model in models
+        if not any([f.filter_by in model.name for f in filters])
+    ]
+
+
+# EXTRACTOR CLASS OVERRIDES
+
+
+class SummarizerFieldFiltered(SummarizerField):
+    """
+    Override `SummarizerField` class with filtered metadata.
+    """
+
+    @property
+    def metadata(self) -> List[SummarizerMetadata]:
+        return filter_models(super().metadata, MetadataFilter)
+
+    def __init__(self, field: Field) -> None:
+        super().__init__(field)
+
+
+class SummarizerResourceFiltered(SummarizerResource):
+    """
+    Override `SummarizerResource` class with filtered fields.
+    """
+
+    @property
+    def fields(self) -> List[SummarizerFieldFiltered]:
+        _fields = [SummarizerFieldFiltered(f) for f in self._value.fields]
+        return filter_models(_fields, FieldFilter)
+
+    def __init__(self, resource: Resource) -> None:
+        super().__init__(resource)
+
+
+# SUMMARIZER CLASS
 
 
 class SummarizerModels:
     """
     Get app, resource, and field data from summarizer database in `dict`
-    format.
+    format. Models are filtered according to filters data.
     """
 
-    def get_all_models() -> List[dict]:
-        """Get all models."""
+    @staticmethod
+    def get_all_resources() -> List[dict]:
+        """Get all resources."""
+
+        resources = filter_models(Resource.query.all(), ResourceFilter)
 
         return [
-            SummarizerResource(model).__dict__
-            for model in Resource.query.all()
+            SummarizerResourceFiltered(resource).__dict__
+            for resource in resources
         ]
 
-    def get_app_models(app_name: str) -> List[dict]:
-        """Get all models for a given app."""
+    @staticmethod
+    def get_app_resources(app_name: str) -> List[dict]:
+        """Get all resources for a given app."""
 
-        app_models = [
-            SummarizerResource(model).__dict__
-            for model in Resource.query.filter_by(app=app_name)
-        ]
+        resources = filter_models(
+            Resource.query.filter_by(app=app_name), ResourceFilter
+        )
 
-        if len(app_models) == 0:
+        # return failure code if no resources exist for app
+        if not resources:
             abort(404, description=f'app "{app_name}" not found')
 
-        return app_models
+        return [
+            SummarizerResourceFiltered(resource).__dict__
+            for resource in resources
+        ]
 
-    def get_model(app_name: str, model_name: str) -> dict:
-        """Get a model from an app."""
+    @staticmethod
+    def get_resource(app_name: str, resource_name: str) -> dict:
+        """Get a resource from an app."""
 
         try:
-            model = Resource.query.filter(
-                (Resource.app == app_name)
-                & (Resource.name.endswith(model_name))
-            ).first()
+            resources = filter_models(
+                Resource.query.filter(
+                    (Resource.app == app_name)
+                    & (Resource.name.endswith(f".{resource_name}"))
+                ),
+                ResourceFilter,
+            )
 
-            return SummarizerResource(model).__dict__
+            # since resource_name should be unique, this should only ever
+            # return one resource
+            resource = resources[0]
+
+            # return failure code if resource is not found
+            if not resource:
+                abort(
+                    404, description=f'resource "{resource_name}" not found'
+                )
+
+            return SummarizerResourceFiltered(resource).__dict__
+
         except AttributeError:
-            abort(404, description=f'model "{model_name}" not found')
+            abort(404, description=f'resource "{resource_name}" not found')
 
-    def get_field(app_name: str, model_name: str, field_name: str) -> dict:
+    @staticmethod
+    def get_field(app_name: str, resource_name: str, field_name: str) -> dict:
         """Get a field from an app model."""
 
         try:
-            field = Field.query.filter(
-                (Field.name == field_name)
-                & (Field.resource.app == app_name)
-                & (Field.resource.name.endswith(model_name))
-            ).first()
-            return SummarizerField(field).__dict__
+
+            # get the named resource
+            resource = SummarizerModels.get_resource(app_name, resource_name)
+
+            # extract the field from the named resource if it exists
+            field = next(
+                (
+                    field
+                    for field in resource["fields"]
+                    if field["name"].endswith(f".{field_name}")
+                ),
+                None,
+            )
+
+            # return failure code if field is not found
+            if not field:
+                abort(404, description=f'field "{field_name}" not found')
+
+            return field
+
         except AttributeError:
             abort(404, description=f'field "{field_name}" not found')
